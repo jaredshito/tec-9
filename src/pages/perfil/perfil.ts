ba0d8a8d97997116b5e7d8fe332a6f403ef53576
jaredshito <raul_debek@hotmail.com>
2017-12-04 20:07:52 -0600

    .

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
+import firebase from 'firebase';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the PerfilPage page.
@@ -15,8 +17,13 @@ import { AuthProvider } from '../../providers/auth/auth';
   templateUrl: 'perfil.html',
 })
 export class PerfilPage {
+  card = {nombreDeUsuario : "", PrimerNombre : "", apellido :""};
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,public authProvider: AuthProvider,
+  public viewCtrl : ViewController) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public authProvider: AuthProvider) {
+    this.obtenerUsuario();
+    console.log(this.card);
   }
 
   ionViewDidLoad() {
@@ -27,5 +34,28 @@ export class PerfilPage {
       this.navCtrl.setRoot('LoginPage');
     });
   }
+  obtenerUsuario(){
+    firebase.database().ref('Usuario/'+firebase.auth().currentUser.uid).on('value', data =>{
+      if(data.val() != null){
+        var datos = data.val();
+        var keys = Object.keys(datos)
+    
+        for(var i = 0; i < keys.length; i++) {
+          var k = keys[i];
+          
+            var datoQuestion = datos[k];
+            if(datoQuestion.nombreUsuario){
+              
+              this.card.nombreDeUsuario = datoQuestion.nombreUsuario;
+              this.card.apellido = datoQuestion.apellido;
+              this.card.PrimerNombre = datoQuestion.primerNombre;
+            }
+        }
+      }
+    });
+  }
+  closeModal() {
+    this.navCtrl.setRoot(HomePage);
+}
 
 }
